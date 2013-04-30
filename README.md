7063c7f1807c32afc50524c82e221f578b6d46da
Kimmy Leo <kenpusney@gmail.com>
2013-05-01 01:01:06 +0800

    init

--- README.md
+++ README.md
@@ -2,3 +2,17 @@ repl-lang
 =========
 
 REPL languages
+
+### Install
+
+```shell
+git clone git://github.com/kenpusney/repl-lang.git
+cd repl-lang && ./repl
+```
+
+### Examples
+
+```ruby
+make :love
+# => "I am making love"
+```
---
+++ repl.rb
@@ -0,0 +1,40 @@
+
+class Object
+	@@env = []
+	def reg(e)
+		if @@env.any? &->(v){ v[0] == self }
+			@@env.delete(@@env.assoc(self))
+		end
+		@@env << [self,e]
+		@@env.uniq!
+		@@env
+	end
+	
+	alias_method :<<,:reg
+	
+	def to_sym
+		self.to_s.to_sym
+	end
+end
+
+module Kernel
+	def make(e,&block)
+		begin
+			if e.class == Class
+				return e.new(&block)
+			elsif e.respond_to?(:make)
+				return e.make(&block)
+			end
+			block.call e
+		rescue => ex
+			e
+		end
+	end
+end
+
+class Symbol
+	def make
+		return "I am making " + self.to_s
+	end
+end
+
